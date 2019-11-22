441263ca0f7ec1cbb0626a96666fe74152b10ece
Dylan Manley <dylan.manley@gmail.com>
2019-11-22 14:33:39 -0800

    add condition service js page and API call

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { DoctorService } from './doctor.js';
+import { DoctorService } from './DoctorService.js';
 
 $(document).ready(function() {
   $('form#userInput').submit(function() {
@@ -16,7 +16,19 @@ $(document).ready(function() {
       getDoctorElements(response);
     })();
     function getDoctorElements(response) {
-      // $('#showGif').attr('src', response.data[randomNumber].images.original.url);
+      $('#showDrAddress').attr('src', response.data[randomNumber].images.original.url);
+      // $("body").css("background-image", `url(${url})`);
+    }
+
+    const condition = $('#condition').val();
+    $('#condition').val("");
+    (async () => {
+      let doctorService = new DoctorService();
+      const response = await doctorService.getDoctor(condition);
+      getDoctorElements(response);
+    })();
+    function getDoctorElements(response) {
+      $('#showDrAddress').attr('src', response.data[randomNumber].images.original.url);
       // $("body").css("background-image", `url(${url})`);
     }
 
