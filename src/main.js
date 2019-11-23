1c2cb30262ece643bd5030a90feba39afc1230c0
Dylan Manley <dylan.manley@gmail.com>
2019-11-22 16:17:20 -0800

    get api call to work and display dr name

--- src/main.js
+++ src/main.js
@@ -19,17 +19,15 @@ $(document).ready(function() {
       getDoctorElements(response);
     })();
     function getDoctorElements(response) {
+      $("#results").show()
       console.log(response.data[0].practices.name);
-      $('#showDrName').text(`Dr. ${response.data[0].profile.last_name}`).val();
-      $('#showDrName').text(`Dr. ${response.data[1].profile.last_name}`).val();
-      $('#showDrName').text(`Dr. ${response.data[2].profile.last_name}`).val();
-      $('#showDrName').text(`Dr. ${response.data[3].profile.last_name}`).val();
-      $('#showDrName').text(`Dr. ${response.data[4].profile.last_name}`).val();
-      $('#showDrName').text(`Dr. ${response.data[5].profile.last_name}`).val();
-      $('#showDrName').text(`Dr. ${response.data[6].profile.last_name}`).val();
-      $('#showDrName').text(`Dr. ${response.data[7].profile.last_name}`).val();
-      $('#showDrName').text(`Dr. ${response.data[8].profile.last_name}`).val();
-      $('#showDrName').text(`Dr. ${response.data[9].profile.last_name}`).val();
+      $('#showDrName').html(`Dr. ${response.data[0].profile.last_name} <br>`).val();
+      console.log(response.data[0].profile);
+      $('#showDrName').html(`Dr. ${response.data[0].profile.last_name} <br>`).val();
+      $('#showDr2Name').html(`Dr. ${response.data[1].profile.last_name} <br>`).val();
+      $('#showDr3Name').html(`Dr. ${response.data[2].profile.last_name} <br>`).val();
+      $('#showDr4Name').html(`Dr. ${response.data[3].profile.last_name} <br>`).val();
+      $('#showDr5Name').html(`Dr. ${response.data[4].profile.last_name} <br>`).val();
       // $('#showDrFirstName').text(`${response.data[0].profile.first_name}`).val();
       // $('#showDrAddress').text(`${response.data[0].profile.address}`).val();
       // $('').attr('src', response.data[0].profile.last_name);
