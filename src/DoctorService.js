83e9ddb850f872d15d693eb90f030f970ff50f7d
Dylan Manley <dylan.manley@gmail.com>
2019-11-24 14:42:08 -0800

    add functionality to main.js to display dr's first name and phone numbers

--- src/DoctorService.js
+++ src/DoctorService.js
@@ -3,6 +3,7 @@ export class DoctorService {
     try {
       let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?user_key=${process.env.API_KEY}&name=${docName}&sort=rating-asc&location=45.523064,-122,100`);
       let jsonifiedResponse = await response.json();
+      console.log(jsonifiedResponse);
       return jsonifiedResponse;
     } catch(error) {
       console.error("There was an error handling your request: " + error.message);
--- src/main.js
+++ src/main.js
@@ -19,13 +19,22 @@ $(document).ready(function() {
     })();
     function getDoctorElements(response) {
       $("#results").show()
-      console.log(response.data[0]);
+      console.log(response.data[5]);
+      $('#showDrFirstName').html("Dr."+" "+`${response.data[0].profile.first_name}`+" ").val();
       $('#showDrName').html(`${response.data[0].profile.last_name} <br>`).val();
-      $('#showDrName').html(`Dr. ${response.data[0].profile.last_name} <br>`).val();
-      $('#showDr2Name').html(`Dr. ${response.data[1].profile.last_name} <br>`).val();
-      $('#showDr3Name').html(`Dr. ${response.data[2].profile.last_name} <br>`).val();
-      $('#showDr4Name').html(`Dr. ${response.data[3].profile.last_name} <br>`).val();
-      $('#showDr5Name').html(`Dr. ${response.data[4].profile.last_name} <br>`).val();
+      $('#showDrPhone').html(`${response.data[0].practices[0].phones[0].number} <br>`).val();
+      $('#showDr2FirstName').html("Dr."+" "+`${response.data[1].profile.first_name}`+" ").val();
+      $('#showDr2Name').html(`${response.data[1].profile.last_name} <br>`).val();
+      $('#showDr2Phone').html(`${response.data[1].practices[0].phones[0].number} <br>`).val();
+      $('#showDr3FirstName').html("Dr."+" "+`${response.data[2].profile.first_name}`+" ").val();
+      $('#showDr3Name').html(`${response.data[2].profile.last_name} <br>`).val();
+      $('#showDr3Phone').html(`${response.data[2].practices[0].phones[0].number} <br>`).val();
+      $('#showDr4FirstName').html("Dr."+" "+`${response.data[3].profile.first_name}`+" ").val();
+      $('#showDr4Name').html(`${response.data[3].profile.last_name} <br>`).val();
+      $('#showDr4Phone').html(`${response.data[3].practices[0].phones[0].number} <br>`).val();
+      $('#showDr5FirstName').html("Dr."+" "+`${response.data[4].profile.first_name}`+" ").val();
+      $('#showDr5Name').html(`${response.data[4].profile.last_name} <br>`).val();
+      $('#showDr5Phone').html(`${response.data[4].practices[0].phones[0].number} <br>`).val();
     }
     // $('#showDrFirstName').text(`Dr. ${response.data[0].profile.first_name}`).val();
     // console.log(response.data[0].profile);
