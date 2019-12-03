8ef8a7fb71c405f01cfc3ed9501840a373646568
Dylan Manley <dylan.manley@gmail.com>
2019-12-02 20:49:31 -0800

    adjust API call so it accounts for medical condition variable

--- src/DoctorService.js
+++ src/DoctorService.js
@@ -1,12 +1,13 @@
 export class DoctorService {
-  async getDoctor(docName) {
+  async getDoctor(docName, conditionInput) {
     try {
-      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?user_key=${process.env.API_KEY}&name=${docName}&sort=rating-asc&location=45.523064,-122,100`);
-      let jsonifiedResponse = await response.json();
-      console.log(jsonifiedResponse);
-      return jsonifiedResponse;
-    } catch(error) {
-      console.error("There was an error handling your request: " + error.message);
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?user_key=${process.env.API_KEY}&name=${docName}&query=${conditionInput}&sort=rating-asc&location=45.523064,-122,100`);
+        let jsonifiedResponse = await response.json();
+        console.log(docName);
+        console.log(conditionInput);
+        return jsonifiedResponse;
+      } catch(error) {
+        console.error("There was an error handling your request: " + error.message);
+      }
     }
   }
-}
